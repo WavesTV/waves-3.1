e8bb4ea53efacf244b8c2f71cca79f73447f6c6e
GageVanK <87737875+GageVanK@users.noreply.github.com>
2023-12-25 16:20:38 +0000

    Pending changes exported from your codespace

--- components/MantineAppShell/MantineFooter/MantineFooter.jsx
+++ components/MantineAppShell/MantineFooter/MantineFooter.jsx
@@ -72,7 +72,7 @@ export function MantineFooter() {
         <SignAndSubmitTx/>
       </Modal>
 
-      <Group position="center" spacing="lg" grow nowrap hiddenFrom="md">
+      <Group position="center" spacing="lg" grow wrap="nowrap" hiddenFrom="md">
         <ActionIcon
           className={classes.link}
           data-active={active === "/" || undefined}
